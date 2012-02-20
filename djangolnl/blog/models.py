b884fa5a012e278a858b6910fdffdc9d84c1812d
Douglas Mendizabal <dmendizabal@denimgroup.com>
2012-02-20 00:03:11 -0600

    Added BlogPost to the admin interface.

--- djangolnl/blog/models.py
+++ djangolnl/blog/models.py
@@ -1,5 +1,6 @@
 import datetime
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 
@@ -7,3 +8,5 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=128)
     body = models.TextField()
     timestamp = models.DateTimeField(default=datetime.datetime.utcnow)
+
+admin.site.register(BlogPost)
